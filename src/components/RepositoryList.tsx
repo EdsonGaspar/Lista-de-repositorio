c841ead1a6ecbb4674602dbd6b878a2c1237c833
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2025-02-14 15:18:34 +0100

    Feat: Consumindo a API, com useEffect

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,28 +1,44 @@
+import { useEffect, useState } from "react";
 import { RepositorioItem } from "./repositorioItem";
 
-interface RepositoryItemProps {
-  name: string;
-  description: string;
-  html_url: string;
-}
+//https://api.github.com/users/EdsonGaspar/repos
 
-export function RepositoryList({
-  name,
-  description,
-  html_url,
-}: RepositoryItemProps) {
+const RepositoryItemProps = {
+  name: "Unform",
+  description: "descrição da unform",
+  html_url: "Caminho",
+};
+
+export function RepositoryList() {
+  const [repositorio, setRepositorio] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/EdsonGaspar/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositorio(data));
+  }, []);
+
+  console.log(repositorio);
+
   return (
     <section className="text-base font-sans text-[rgb(18,18,20)]">
       <h1 className="text-2xl font-bold mb-2">Lista de repositórios</h1>
-      <ul>
-        <li>
-          <strong>{name}</strong>
-          <p>{description}</p>
-          <a href={html_url}>{"Acessar repositório"}</a>
-        </li>
-        <RepositorioItem />
-        <RepositorioItem />
-        <RepositorioItem />
+      <ul className="flex flex-col space-y-4">
+        <RepositorioItem
+          nome={RepositoryItemProps.name}
+          description={RepositoryItemProps.description}
+          html_url={RepositoryItemProps.html_url}
+        />
+        <RepositorioItem
+          nome={RepositoryItemProps.name}
+          description={RepositoryItemProps.description}
+          html_url={RepositoryItemProps.html_url}
+        />
+        <RepositorioItem
+          nome={RepositoryItemProps.name}
+          description={RepositoryItemProps.description}
+          html_url={RepositoryItemProps.html_url}
+        />
       </ul>
     </section>
   );
--- src/components/repositorioItem.tsx
+++ src/components/repositorioItem.tsx
@@ -1,15 +1,22 @@
 import { ComponentProps } from "react";
-import { Counter } from "./counter";
 
-interface RepositorioItemProps extends ComponentProps<"li"> {}
+interface RepositorioItemProps extends ComponentProps<"li"> {
+  nome: string;
+  description: string;
+  html_url: string;
+}
 
 export function RepositorioItem({ ...props }: RepositorioItemProps) {
   return (
     <li>
-      <h2 className="font-bold text-[18px]">{props.children}</h2>
-      <p>Descrição do repositorio Unform </p>
-      <a href="">Acessar Repositoro</a>
-      <Counter />
+      <h2 className="font-bold text-[18px]">{props.nome}</h2>
+      <p className="text-sm">{props.description} </p>
+      <a
+        href={props.html_url}
+        className="text-lg border border-blue-500 text-blue-500 rounded py-1 px-4 mt-4 inline-block"
+      >
+        Acessar
+      </a>
     </li>
   );
 }
